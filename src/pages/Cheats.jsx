df1fa09c9e70810258c14341a06b9de49636babe
Blake181041 <Blakeryan1418@gmail.com>
2026-05-01 23:23:43 -0400

    Fix Cheats component to return CheatsLayout

--- src/pages/Cheats.jsx
+++ src/pages/Cheats.jsx
@@ -1,7 +1,9 @@
 import CheatsLayout from "../layouts/Cheats";
 import { memo } from "react";
 
-const Cheats = memo(() => { <CheatsLayout />; });
+const Cheats = memo(() => {
+  return <CheatsLayout />;
+});
 
 Cheats.displayName = 'Cheats';
 export default Cheats;
